81231781c546bb9e68b704bf87e14c9bbe5286ab
CristianMorenoMo <ccmorenomojica@gmail.com>
2021-03-01 20:39:21 -0500

    update last_fm

--- last_fm/last_fm/items/models.py
+++ last_fm/last_fm/items/models.py
@@ -7,35 +7,35 @@ from django.contrib.auth.models import User
 class Artist(models.Model):
 	id_art = models.CharField(blank=True,primary_key=True,max_length=255)
 	name_art = models.CharField(('name_art'),blank=True, max_length=255)
-	def _str_(self):
-		return"()".format(self.name_art)
+	def __str__(self):
+		return"{}".format(self.name_art)
 
 class Items(models.Model):
 	id_item = models.CharField(blank=True,primary_key=True,max_length=255)
 	#artist = models.ForeignKey(Artist,related_name="id_artista_items",on_delete=models.CASCADE)
 	name_item = models.CharField(('name_item'),blank=True, max_length=255)
-	def _str_(self):
-		return"()".format(self.name_item)
+	def __str__(self):
+		return"{}".format(self.name_item)
 
 class Picture_item(models.Model):
 	item = models.ForeignKey(Items,related_name="picture_items",on_delete=models.CASCADE)
 	picture_item = models.ImageField(upload_to = "item_picture")
-	def _str_(self):
-		return"()".format(self.id_item)
+	def __str__(self):
+		return"{}".format(self.id_item)
 
 class Picture_art(models.Model):
 	artist = models.ForeignKey(Artist,related_name = "picture_artist",on_delete=models.CASCADE)
 	picture_art = models.ImageField(upload_to = "artist_picture")
-	def _str_(self):
-		return"()".format(self.id_art)
+	def __str__(self):
+		return"{}".format(self.id_art)
 
 class tx_item(models.Model):
 	#id_user = models.ForeignKey(User,related_name = "tx_user",on_delete=models.CASCADE)
 	artist = models.ForeignKey(Artist,related_name = "tx_artist",on_delete=models.CASCADE)
 	item = models.ForeignKey(Items,related_name="tx_items",on_delete=models.CASCADE)
 	date_tx =  models.DateTimeField(auto_now=True)
-	def _str_(self):
-		return"()".format(self.id_art,self.id_item)
+	def __str__(self):
+		return"{}".format(self.id_art,self.id_item)
 
 
 
@@ -51,4 +51,4 @@ class Profile(models.Model):
 
     def __str__(self):
         """Return username"""
-        return self.user
+        return "{}".format(self.user)
